1d6c4d0f4506416de01fb40507a6e20069efaa27
Tsai-TsungLin <ss19960429ss@gmail.com>
2023-07-25 16:15:34 +0800

    [fix] 修改在opensearch 才套用index/data 產生result

--- help.go
+++ help.go
@@ -2,6 +2,9 @@ package opensearch
 
 import (
 	"encoding/json"
+	"log"
+	"regexp"
+	"strings"
 	"time"
 )
 
@@ -34,4 +37,73 @@ func actionUpdate(index, id string) ActionUpdate {
 
 func contentDetailUpdate(data InsertData) UpdateData {
 	return UpdateData{Doc: data}
+}
+
+//把data 轉成 字串
+func DataCompression(data map[string]interface{}, index string) string {
+
+	if len(data) == 0 {
+		return ""
+	}
+
+	index = indexRegexp(index)
+
+	result, err := BulkCreateRemoteWrite(index, data)
+	if err != nil {
+		log.Println("Bulk Create error: ", err)
+		return ""
+	}
+
+	return result
+}
+
+func indexRegexp(index string) string {
+
+	pattern := `%\{YYYY([-.\/]MM([-.\/]DD)?)?\}`
+
+	// 編譯正則表達式
+	re, _ := regexp.Compile(pattern)
+
+	// 檢查這個參數是否符合正則表達式的模式
+	matches := re.FindStringSubmatch(index)
+
+	if len(matches) > 0 {
+		// 基於匹配的字符串決定需要的日期格式
+		var layout string
+		var separator string
+		hasSeparator := false
+
+		// 獲取日期部分和分隔符
+		datePart := strings.Trim(matches[0], "%{}")
+		if strings.Contains(datePart, "-") {
+			separator = "-"
+			hasSeparator = true
+		} else if strings.Contains(datePart, ".") {
+			separator = "."
+			hasSeparator = true
+		} else if strings.Contains(datePart, "/") {
+			separator = "-"
+			hasSeparator = true
+		}
+
+		// 獲取日期格式
+		if hasSeparator {
+			switch strings.Count(datePart, separator) {
+			case 1:
+				layout = "2006" + separator + "01"
+			case 2:
+				layout = "2006" + separator + "01" + separator + "02"
+			}
+		} else {
+			layout = "2006"
+		}
+
+		// 獲取當前的日期
+		currentDate := time.Now().Format(layout)
+
+		// 將匹配的部分替換為當天的日期
+		index = re.ReplaceAllString(index, currentDate)
+	}
+
+	return index
 }
---
+++ opensearch_unused.go
@@ -0,0 +1,234 @@
+package opensearch
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/opensearch-project/opensearch-go"
+	"github.com/opensearch-project/opensearch-go/opensearchapi"
+)
+
+//建立Index
+func CreateIndex(client *opensearch.Client, IndexName string) error {
+	//設定Index
+	settings := strings.NewReader(`{
+		"settings": {
+	    	"index": {
+	        	"number_of_shards": 1,
+	        	"number_of_replicas": 2
+	        }
+	    },
+		"mappings": {
+			"properties": {
+				"timestamp": {
+					"type": "date"
+				}
+			}
+		}
+	}`)
+
+	res := opensearchapi.IndicesCreateRequest{
+		Index: IndexName,
+		Body:  settings,
+	}
+
+	createIndexResponse, errCreateIndex := res.Do(context.Background(), client)
+	if errCreateIndex != nil {
+		log.Println("failed to create index ", errCreateIndex)
+		return errCreateIndex
+	}
+
+	defer createIndexResponse.Body.Close()
+
+	log.Println(createIndexResponse)
+	return nil
+}
+
+//單一插入
+func SingleInsert(client *opensearch.Client, index, document string) error {
+	req := opensearchapi.IndexRequest{
+		Index: index,
+		Body:  strings.NewReader(document),
+	}
+	insertResponse, err := req.Do(context.Background(), client.Transport)
+	if err != nil {
+		log.Println("failed to insert document: ", err)
+		return err
+	}
+	defer insertResponse.Body.Close()
+
+	log.Println(insertResponse)
+	log.Println("add success")
+	return nil
+}
+
+//單一刪除
+func SingleDeleteIndex(client *opensearch.Client, index []string) error {
+	deleteIndex := opensearchapi.IndicesDeleteRequest{
+		Index: index,
+	}
+
+	deleteIndexResponse, errDeleteIndex := deleteIndex.Do(context.Background(), client.Transport)
+	if errDeleteIndex != nil {
+		log.Println("failed to delete index ", errDeleteIndex)
+		return errDeleteIndex
+	}
+	defer deleteIndexResponse.Body.Close()
+
+	log.Println(deleteIndexResponse)
+
+	return nil
+
+}
+
+//Search something
+func Search(client *opensearch.Client, Index, key, value string) (result SearchResponse, err error) {
+
+	var res *opensearchapi.Response
+
+	if key != "" && value != "" {
+		queryString := fmt.Sprintf(`%s: "%s"`, "*", value)
+
+		res, err = client.Search(
+			client.Search.WithIndex(Index),
+
+			client.Search.WithQuery(queryString),
+		)
+		if err != nil {
+			log.Printf("error Search: [%s]", err.Error())
+		}
+	} else {
+		res, err = client.Search(
+			client.Search.WithIndex(Index),
+			client.Search.WithSize(5),
+		)
+		if err != nil {
+			log.Printf("error Search: [%s]", err.Error())
+		}
+	}
+
+	// log.Println(res.StatusCode)
+	if res.StatusCode > 299 {
+		var errorResult SearchErrorResponse
+		json.NewDecoder(res.Body).Decode(&errorResult)
+		return result, errors.New(errorResult.Error.Reason)
+	}
+
+	// log.Printf("search response: [%+v]", res)
+
+	json.NewDecoder(res.Body).Decode(&result)
+
+	return result, nil
+}
+
+func BulkPrevious(client *opensearch.Client, mode string, data BulkPreviousUse) (result *opensearchapi.Response, err error) {
+
+	switch mode {
+	case "create":
+		if data.Create.Data == nil || data.Create.Index == "" {
+			errCreateData := errors.New("create issue is illegal,please check it ")
+			return nil, errCreateData
+		}
+		createData, errCreate := BulkCreate(data.Create.Index, data.Create.Data)
+		if errCreate != nil {
+			return nil, errCreate
+		}
+
+		result, errExecute := BulkExecute(client, createData)
+		if errExecute != nil {
+			return nil, errExecute
+		}
+
+		return result, nil
+
+	case "update":
+		if data.Update.Id == "" || data.Update.Index == "" || len(data.Update.Data.Data) == 0 {
+			errUpdateData := errors.New("update issue is illegal,please check it ")
+			return nil, errUpdateData
+		}
+		updateData, errUpdate := BulkUpdate(data.Update.Index, data.Update.Id, data.Update.Data)
+		if errUpdate != nil {
+			return nil, errUpdate
+		}
+
+		result, errExecute := BulkExecute(client, updateData)
+		if errExecute != nil {
+			return nil, errExecute
+		}
+
+		return result, nil
+
+	case "delete":
+		if len(data.Delete) == 0 {
+			errDeleteData := errors.New("delete issue is illegal,please check it")
+			return nil, errDeleteData
+		}
+		deleteData, errDelete := BulkDelete(data.Delete)
+		if errDelete != nil {
+			return nil, errDelete
+		}
+
+		result, errExecute := BulkExecute(client, deleteData)
+		if errExecute != nil {
+			return nil, errExecute
+		}
+
+		return result, nil
+
+	default:
+		return nil, errors.New("invalid mode")
+	}
+
+}
+
+func BulkDelete(Delete map[string]string) (result string, err error) {
+
+	r := []interface{}{}
+
+	for key, value := range Delete {
+		deleteIndex := actionDelete(key, value)
+		r = append(r, deleteIndex)
+	}
+
+	buf := &bytes.Buffer{}
+	enc := json.NewEncoder(buf)
+
+	for _, v := range r {
+		if errEncode := enc.Encode(v); errEncode != nil {
+			log.Fatal(err)
+			return "", errEncode
+		}
+	}
+
+	return buf.String(), nil
+
+}
+
+func BulkUpdate(index, id string, data InsertData) (result string, err error) {
+
+	r := []interface{}{}
+
+	Action := actionUpdate(index, id)
+
+	ContentDetail1 := contentDetailUpdate(data)
+
+	r = dataMix(r, Action, ContentDetail1)
+
+	buf := &bytes.Buffer{}
+	enc := json.NewEncoder(buf)
+
+	for _, v := range r {
+		if errEncode := enc.Encode(v); errEncode != nil {
+			log.Fatal(err)
+			return "", errEncode
+		}
+	}
+
+	return buf.String(), nil
+
+}
